8803c5fe081af0a4500ab1a187e4cc91f641bea1
Sandip Ghadge <sandip.ghadge@wwindia.com>
2019-10-31 23:41:35 +0530

    added nyc coverage

--- test/controllers/user.controller.spec.ts
+++ test/controllers/user.controller.spec.ts
@@ -1,35 +1,65 @@
-import { expect, should } from 'chai';
+import { expect } from 'chai';
 import * as dotenv from 'dotenv';
+import * as http from 'http';
 import * as request from 'supertest';
+import { App } from './../../App';
+import { logger } from './../../src/logger';
 
 dotenv.config({
   path: '.env.test',
 });
 
-// let server: http.Server;
-// let app: App;
-const server: string = 'http://localhost:3000';
+let server: http.Server;
+let app: App;
 const commonHeaders: any = {Authorization: ''};
+
 before(async () => {
-  const body: any = {
-    email: 'sandip@yopmail.com',
-    password: 'sandip123',
-  };
-  const users: any = await request(server).post('/api/auth/login').send(body);
-  commonHeaders.Authorization = users.body.data.token;
+  app = new App();
+  await app
+    .init()
+    .then(() => {
+      server = app.httpServer;
+      server.on('error', function(): void {
+        // logger.log('testing server ');
+      });
+      server.on('listening', function(): void {
+        // logger.info('testing server started');
+      });
+      server.listen(process.env.PORT);
+    })
+    .then(async () => {
+        // call login api
+        const body: any = {
+            email: 'admin-shopping@yopmail.com',
+            password: 'admin123',
+        };
+        const users: any = await request(server).post('/api/auth/login').send(body);
+        commonHeaders.Authorization = users.body.data.token;
+    })
+    .catch((err: Error) => {
+      logger.error(err.name);
+      logger.error(err.message);
+      logger.error(err.stack);
+    });
 });
 
 describe('User module', () => {
   describe('"usercontroller.getUsers()"', () => {
-    it('should list users', async () => {
-      const users: any = await request(server).get('/api/users').set(commonHeaders);
-      expect(users.body.success, 'success').to.be.equal(true);
-      expect(users.body.message).to.be.equal('Success');
-      expect(users.body.data).to.be.a('array');
-      expect(users.body.data[0]).to.be.a('object');
-      expect(users.body.data[0]).to.have.property('userRole');
-      expect(users.body.data[0]).to.have.property('email');
-      expect(users.body.data[0]).to.have.property('first_name');
+    it('should should list users', async () => {
+      try {
+        logger.info({token_found : commonHeaders});
+        const users: any = await request(server).get('/api/users').set(commonHeaders);
+        expect(users.body.success, 'success').to.be.equal(true);
+        expect(users.body.message).to.be.equal('Success');
+        expect(users.body.data).to.be.a('array');
+        expect(users.body.data[0]).to.be.a('object');
+        expect(users.body.data[0]).to.have.property('userRole');
+        expect(users.body.data[0]).to.have.property('email');
+        expect(users.body.data[0]).to.have.property('first_name');
+
+      } catch (err) {
+        expect(err.statusCode).to.be.equal(401);
+      }
     });
   });
 
@@ -46,33 +76,16 @@ describe('User module', () => {
     });
   });
 
-  describe('"usercontroller.updateUser()"', () => {
-    const id: string = '5d1cb16ecca2b968536aa52c';
-    const body: any = {
-      email: 'sandip@yopmail.com',
-      password: 'sandipg123',
-      first_name: 'Sandip',
-      last_name: 'Ghadge',
-    };
-    it('should update a user', async () => {
-      const users: any = await request(server).put(`/api/users/${id}`).set(commonHeaders).send(body);
-      expect(users.body.success, 'success').to.be.equal(true);
-      expect(users.body.message).to.be.equal('Success');
-      expect(users.body.data).to.be.a('object');
-      expect(users.body.data).to.have.property('userRole');
-      expect(users.body.data).to.have.property('email');
-      expect(users.body.data).to.have.property('first_name');
-    });
-  });
-
-  // describe('"usercontroller.deleteUser()"', () => {
+  // describe('"usercontroller.updateUser()"', () => {
   //   const id: string = '5d1cb16ecca2b968536aa52c';
   //   const body: any = {
   //     email: 'sandip@yopmail.com',
   //     password: 'sandipg123',
+  //     first_name: 'Sandip',
+  //     last_name: 'Ghadge',
   //   };
-  //   it('should delete a user', async () => {
-  //     const users: any = await request(server).delete(`/api/users/${id}`).set(commonHeaders).send(body);
+  //   it('should update a user', async () => {
+  //     const users: any = await request(server).put(`/api/users/${id}`).set(commonHeaders).send(body);
   //     expect(users.body.success, 'success').to.be.equal(true);
   //     expect(users.body.message).to.be.equal('Success');
   //     expect(users.body.data).to.be.a('object');
@@ -81,4 +94,5 @@ describe('User module', () => {
   //     expect(users.body.data).to.have.property('first_name');
   //   });
   // });
+
 });
